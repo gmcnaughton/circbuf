ab94365a3e439928cb935844228e76972bcef39e
Gordon McNaughton <gmcnaughton@gmail.com>
2017-06-13 11:36:16 -0400

    Update comments

--- circbuf.go
+++ circbuf.go
@@ -1,7 +1,7 @@
 package circbuf
 
 // Circbuf is a simple circular buffer with a predefined capacity. It works
-// like a slice, but once full it wraps.  Operations on this object are not
+// like a slice, but wraps when full. Operations on this object are not
 // thread-safe.
 type Circbuf struct {
 	items []interface{}
@@ -19,7 +19,7 @@ func New(cap int) *Circbuf {
 }
 
 // Add appends an item to the buffer. If the buffer is full (length ==
-// capacity), the first item in the buffer will be ejected to make room.
+// capacity), the first item is removed to make room.
 func (c *Circbuf) Add(e interface{}) {
 	c.items[(c.head+c.len)%c.cap] = e
 	if c.len < c.cap {
@@ -37,12 +37,12 @@ func (c *Circbuf) Do(f func(interface{})) {
 	}
 }
 
-// Len returns the number of items.
+// Len returns the number of items in the buffer.
 func (c *Circbuf) Len() int {
 	return c.len
 }
 
-// Cap returns the number of slots.
+// Cap returns the number of items the buffer can hold before wrapping.
 func (c *Circbuf) Cap() int {
 	return c.cap
 }
